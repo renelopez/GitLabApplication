1c773c06a3bbc336282b4fb7b485a67e4ae1ead3
rene.lopez <rene.lopez.cano@gmail.com>
2016-09-14 10:04:50 -0500

    Initial Jenkins commit

---
+++ GitLabApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitLabApplication.Startup))]
+namespace GitLabApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
